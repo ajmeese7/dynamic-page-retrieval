7794bc5f32392dd59aa79475ad906fbd33042142
Aaron Meese <ajmeese7@gmail.com>
2018-11-10 16:57:12 -0700

    Project successful
    Now it does everything it is supposed to, but I need to deploy it to Heroku from the repo once I commit this to triple-check. Still need to go back and make it all fancy if it proceeds as planned; this includes making a README with instructions to replicate.

--- index.js
+++ index.js
@@ -1,51 +1,34 @@
-var webdriver = require('selenium-webdriver');
+const puppeteer = require('puppeteer')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 var app = express()
-var By = webdriver.By;
 
 app
-  .use(express.static(path.join(__dirname, 'public')))
+  // NOTE: Use this section if you want to make the site fancy as well as retrieve code
+  /*.use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.render('pages/index'))*/
   .get('/scrape', function (req, res) {
       var url = require('url');
       var parameter_URL = req.query.URL; // 'URL' is the parameter in the page location (after the '?')
 
-      console.log("URL parameter is... " + parameter_URL);
-
-      // TODO: Test if it actually works with JS content (the main goal of the project)
-      var phantomjs = require('phantomjs-prebuilt')
-      var webdriverio = require('webdriverio')
-      var wdOpts = { desiredCapabilities: { browserName: 'phantomjs' } }
-
-      phantomjs.run('--webdriver=4444').then(program => {
-        webdriverio.remote(wdOpts).init()
-          .url(parameter_URL)
-          .getTitle().then(title => {
-            console.log(page.content)
-            program.kill() // quits PhantomJS
-          })
-      })
-
-      /*var driver = new webdriver.Builder()
-          .forBrowser('phantomjs')
-          .build();
-      driver.get('http://www.google.com/ncr');
-      driver.findElement(By.name('q')).sendKeys('webdriver');
-      driver.findElement(By.name('btnG')).click();
-      driver.wait(function() {
-          return driver.getTitle().then(function(title) {
-              console.log(title);
-              return title === 'webdriver - Google Search';
+      puppeteer
+        .launch({args: ['--no-sandbox', '--disable-setuid-sandbox']})
+        .then(function(browser) {
+          return browser.newPage();
+        })
+        .then(function(page) {
+          return page.goto(parameter_URL).then(function() {
+            return page.content();
           });
-      }, 5000).then(function() {
-          res.status(200).send('Done');
-      }, function(error) {
-          res.status(200).send(error);
-      });
-      driver.quit();*/
+        })
+        .then(function(html) {
+          console.log(html);
+        })
+        .catch(function(err) {
+          console.error(err);
+        });
   })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`)) // localhost:5000
